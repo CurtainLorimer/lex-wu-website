280b62ea63702f32e7ab91d4987c3b23a23a40c6
lwu877 <67766822+lwu877@users.noreply.github.com>
2025-01-05 23:26:06 -0500

    updated Astro

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,4 @@
 
 export default new Map([
-['src/content/work/curtain-lorimer/index.mdx', () => import('astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fcurtain-lorimer%2Findex.mdx&astroContentModuleFlag=true')]]);
+["src/content/work/curtain-lorimer/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fcurtain-lorimer%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
